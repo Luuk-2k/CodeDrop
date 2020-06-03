393406fdae646c281dc5e9eb50fc162960bd22b7
Redice <you@example.com>
2020-06-03 18:24:02 +0200

    yes

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Code Drop</title>
+    <link href="main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Assistant&family=Montserrat&family=Open+Sans&family=Roboto&display=swap"
+          rel="stylesheet">
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<section>
+    <nav class="navbar">
+        <a class="logo" href="#"> <img class="x" height="60" src="image/bg-19.svg"
+                                       width="290" alt="x"></a>
+        <ul class="nav-links">
+            <li class="nav-item us"><a href="#">Why us?</a></li>
+            <li class="nav-item"><a href="#">idk</a></li>
+            <li class="nav-item"><a href="#">idk</a></li>
+            <li class="nav-item"><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <h1 class="tile">Welcome to Code Drop</h1>
+    <p class="introduction">"We are a community aimed at uniting all kinds of gamers. Join us as we carve a place on the
+        internet fellow fans to make new friendships, discuss your thoughts/favorite games, and get your game on"</p>
+    <div class="join-button-cont row">
+        <div class="di hvr-grow "><h5 class="di-title">You've been invited to join a server</h5>
+            <div class="di-content di-flex">
+                <div class="di-icon"
+                     style="background-image: url(&quot;https://cdn.discordapp.com/icons/491263097323913246/982fb3417757c577ce45dcf1d6893f9c.webp?size=128&quot;);"></div>
+                <div class="di-details di-flex">
+                    <div class="di-d-name">Code Drop Discord</div>
+                    <div class="di-d-details"><i class="di-d-d-online"></i><span>127 Online  </span><i
+                                class="di-d-d-offline"></i><span> 813 Members</span></div>
+                </div>
+                <div class="di-break"></div>
+                <a href="https://discord.gg/otaku" target="_blank" rel="noopener noreferrer" type="button"
+                   class="di-button">Join</a></div>
+        </div>
+    </div>
+    <div class="relative squiggle-holder">
+        <div>
+            <div class="apng hidden-sm"></div>
+            <div class="text-center below-title hidden-sm">
+                <div class="texty mask-top"><span class=""
+                                                  style="opacity: 1; transform: translate(0px, 0%);"></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span><span
+                            style="opacity: 1; transform: translate(0px, 0%);" class=""></span></div>
+            </div>
+        </div>
+        <svg preserveAspectRatio="none" viewBox="0 0 1920 250" version="1.1" class="squiggle">
+            <path fill="#161616" fill-opacity="1"
+                  d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+        </svg>
+    </div>
+</section>
+
+<div class="whyus">
+    <h1>Tomorrow i continue</h1>
+</div>
+<script>
+
+    // onlick scroll
+    $(".us").click(function () {
+        $('html,body').animate({
+                scrollTop: $(".whyus").offset().top
+            },
+            'slow');
+    });
+</script>
+
+</body>
+</html>
